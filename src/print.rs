a5ba44b9ed566cebb368111619ac7971ef6b29fd
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-11-03 09:19:20 -0100

    worked through formatting

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,10 @@
 pub fn run(){
     // print to console
     println!("Hello from the print.rs file");
+
+    //When printing something we always need a placeholder, can't just plug in interger
+    println!("Number: {}", 1);
+
+    //Multiple placeholders
+    println!("{} is from {}", "Brad", "Mass");
 }
