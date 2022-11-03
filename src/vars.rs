144986ae805a12b25b7dd4ccb6a42af17383df4d
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-11-03 10:14:45 -0100

    worked through variables, how to assign and reassign them

--- src/vars.rs
+++ src/vars.rs
@@ -11,4 +11,12 @@ let mut age=35;
 println!("My name is {} and I am {}", name, age);
 age=36;
 println!("My name is {} and I am {}", name, age);
+
+// Define constant - you need to add a type i.e-i32 = interger 32 bit
+const ID: i32 = 001;
+println!("ID: {}", ID);
+
+//Assign multiple vars
+let(my_name, my_age )= ("Sara", 35);
+println!("{} is {}", my_name, my_age);
 }
